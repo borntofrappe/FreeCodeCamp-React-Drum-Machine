d95d87ea189ab7198c34c53833195de897977d4f
gabrielecorti <borntofrappe@gmail.com>
2018-08-06 14:30:16 +0200

    include files replicating the application UI

---
+++ src/AppOutput.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './css/AppOutput.css';
+
+/*
+render a single div in which to display the output of the application 
+output passed as property from App.js and accessible under props.output
+*/
+const AppOutput = (props) =>  {
+  return(
+    <div className="AppOutput">
+      {props.output}
+    </div>
+  );
+}
+
+export default AppOutput;
